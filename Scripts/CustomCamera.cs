d04afa4ca9cc986a0f0d2b0aacca69ab8494011b
DESKTOP-FGAJHSJ\mhoow <mhoow6@gmail.com>
2021-04-23 21:30:17 +0900

    카메라

---
+++ Scripts/CustomCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCamera : MonoBehaviour
+{
+    public Transform playerTransform;
+    public ResourceManager resourceManager;
+
+    Vector3 offset;
+
+    private void Awake()
+    {
+        // Waiting for Resource
+        if (playerTransform == null)
+        {
+            Debug.Log("[Custom Camera] Waiting for Resource Manager..");
+        }
+
+        // Get transform from Resource Manager
+        Invoke("CatchPlayer", 1);
+
+    }
+
+    private void LateUpdate()
+    {
+        if (playerTransform != null)
+            transform.position = playerTransform.position + offset;
+    }
+
+    void CatchPlayer()
+    {
+        playerTransform = resourceManager.player.transform;
+        offset = transform.position - playerTransform.position;
+        Debug.Log("[Custom Camera] Player attached.");
+    }
+}
